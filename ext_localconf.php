6d4d0310cdeb7e1496f34c79faf0b68b75c733d3
Stefan Froemken <froemken@gmail.com>
2020-02-17 15:58:46 +0100

    Plugin Preview
    * Add kk downloader into NewContentElementWizard
    * Add Plugin Preview

--- ext_localconf.php
+++ ext_localconf.php
@@ -29,4 +29,20 @@ tt_content.list.20.kkdownloader_pi1 = < plugin.tx_kkdownloader_pi1
         ,
         'defaultContentRendering'
     );
+
+    // Register SVG Icon Identifier
+    $svgIcons = [
+        'ext-kkdownloader-wizard-icon' => 'plugin_wizard.gif',
+    ];
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    foreach ($svgIcons as $identifier => $fileName) {
+        $iconRegistry->registerIcon(
+            $identifier,
+            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+            ['source' => 'EXT:kk_downloader/Resources/Public/Icons/' . $fileName]
+        );
+    }
+
+    // add kk_downloader plugin to new element wizard
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:kk_downloader/Configuration/TSconfig/ContentElementWizard.txt">');
 });
