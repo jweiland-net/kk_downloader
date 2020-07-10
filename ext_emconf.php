dfc092a948c8a8c3ca8f7abd079594a33e0cd7e7
Stefan Froemken <froemken@gmail.com>
2020-07-10 08:19:06 +0200

    Remove TYPO3 8 compatibility. Add TYPO3 10 compatibility.

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,20 +1,9 @@
 <?php
-
-/***************************************************************
- * Extension Manager/Repository config file for ext "kk_downloader".
- *
- * Auto generated 23-09-2016 17:16
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Simple download-system with counter and categories',
     'description' => 'Download system with counter, simple category management, sorting criteria and page browsing in the LIST-view. Configuration via flexforms and HTML template. (example: http://kupix.de/downloadlist.html)',
     'category' => 'plugin',
-    'version' => '5.0.1',
+    'version' => '6.0.0',
     'state' => 'stable',
     'uploadfolder' => true,
     'createDirs' => '',
@@ -24,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'jweiland.net',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.14-9.5.99',
+            'typo3' => '9.5.17-10.4.99',
         ],
         'conflicts' => [
         ],
