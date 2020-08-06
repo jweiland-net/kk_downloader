56c8a1796617381722426ebe24e9d54b4cf26fac
Stefan Froemken <froemken@gmail.com>
2020-08-06 08:29:34 +0200

    Add Extbase table mapping for TYPO3 10

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,34 @@
+<?php
+return [
+    \JWeiland\KkDownloader\Domain\Model\Download::class => [
+        'tableName' => 'tx_kkdownloader_images',
+        'properties' => [
+            'title' => [
+                'fieldName' => 'name'
+            ],
+            'files' => [
+                'fieldName' => 'image'
+            ],
+            'preview' => [
+                'fieldName' => 'imagepreview'
+            ],
+            'longDescription' => [
+                'fieldName' => 'longdescription'
+            ],
+            'downloadDescription' => [
+                'fieldName' => 'downloaddescription'
+            ],
+            'categories' => [
+                'fieldName' => 'cat'
+            ],
+        ],
+    ],
+    \JWeiland\KkDownloader\Domain\Model\Category::class => [
+        'tableName' => 'tx_kkdownloader_cat',
+        'properties' => [
+            'title' => [
+                'fieldName' => 'cat'
+            ],
+        ],
+    ],
+];
