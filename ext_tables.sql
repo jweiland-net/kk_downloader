343fa4c551637932d1e63851774f732186f98a57
Stefan Froemken <sfroemken@jweiland.net>
2024-03-06 11:22:40 +0100

    Better structure of ext_tables.sql file

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,13 +1,14 @@
 #
 # Table structure for table 'tx_kkdownloader_images'
 #
-CREATE TABLE tx_kkdownloader_images (
-	name tinytext,
-	image int(11) unsigned DEFAULT '0',
-	imagepreview int(11) unsigned DEFAULT '0',
-	description text,
-	longdescription text,
-	clicks int(10) DEFAULT '0' NOT NULL,
-	last_downloaded int(11) DEFAULT '0' NOT NULL,
+CREATE TABLE tx_kkdownloader_images
+(
+	name             tinytext,
+	image            int(11) unsigned DEFAULT '0',
+	imagepreview     int(11) unsigned DEFAULT '0',
+	description      text,
+	longdescription  text,
+	clicks           int(10) DEFAULT '0' NOT NULL,
+	last_downloaded  int(11) DEFAULT '0' NOT NULL,
 	ip_last_download varchar(45) DEFAULT '' NOT NULL
 );
